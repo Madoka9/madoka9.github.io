68a530584affe70a1b1d5da93f67c5f67f47b0e4
Home <1521070145@qq.com>
2026-02-22 21:51:13 +0800

    Site updated: 2026-02-22 21:51:12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1771766967606";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1771768264682";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
